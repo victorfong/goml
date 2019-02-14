4e0dc9864100a3a597bec0d9c9a7553f788d4d3c
Victor Fong <victor.fong@cognizant.com>
2019-02-13 20:07:43 -0500

    Add initial test

---
+++ matrix_test.go
@@ -0,0 +1,22 @@
+package main_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Matrix Operations", func() {
+
+	BeforeEach(func() {
+		// do nothing yet
+	})
+
+	Describe("Multiplication", func() {
+		Context("when 2 matrices multiplies", func() {
+
+			It("should return product", func() {
+				Expect(true).To(BeTrue())
+			})
+		})
+	})
+})
